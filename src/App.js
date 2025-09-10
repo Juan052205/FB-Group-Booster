a98d56e9e90a1a3ff54b9f71b53c803be25c1ad5
Juan052205 <103338934+Juan052205@users.noreply.github.com>
2025-09-09 22:38:46 -0500

    Add React App.js base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Import React
+import React from 'react';
+
+// Create a functional component
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+// Export the component
+export default App;
